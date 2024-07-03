79d139aba6b9fdf940c38d93feee932411ad58a1
David Pérez Martínez <taborlindp@gmail.com>
2024-07-03 16:45:36 +0200

    add styles

--- src/components/cam-button.ts
+++ src/components/cam-button.ts
@@ -2,7 +2,7 @@ import { LitElement, html } from "lit";
 
 export class CamButton extends LitElement {
   render() {
-    return html`<button>Take a picture</button>`;
+    return html`<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Take a picture</button>`;
   }
 }
 
